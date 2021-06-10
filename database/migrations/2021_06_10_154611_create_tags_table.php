a41a3701a36d355b517169430e1218c9b11b5925
Michele Lisignoli <michele@lisignoli.com>
2021-06-10 17:48:39 +0200

    recreated the tags table

---
+++ database/migrations/2021_06_10_154611_create_tags_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTagsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tags', function (Blueprint $table) {
+            $table->id();
+            $table->string('tagCourse', 25);
+            $table->string('allergens');
+            $table->boolean('veggie')->default(0);
+            $table->boolean('vegan')->default(0);
+            $table->boolean('spicy')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tags');
+    }
+}
