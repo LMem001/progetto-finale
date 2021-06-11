c0b658fa06b4b119f3012a1424320f231459db3e
Luca <memeoluca6@gmail.com>
2021-06-11 12:20:14 +0200

    changed iban and billling_address length

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,8 +20,8 @@ class CreateUsersTable extends Migration
             $table->string('email', 50)->unique();
             $table->string('phone', 20)->unique();
             $table->string('vat', 30)->unique();
-            $table->string('billing_address', 30);
-            $table->string('iban', 27)->unique();
+            $table->string('billing_address', 100);
+            $table->string('iban', 40)->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
