736d31c0a1055959bcd93c47575ef49b7aa789ba
Michele Lisignoli <michele@lisignoli.com>
2021-06-11 13:05:36 +0200

    Created Seeder_restaurants

---
+++ database/seeds/RestaurantsTableSeeder.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\User;
+use App\Restaurant;
+use Faker\Generator as Faker;
+use Illuminate\Support\Str;
+
+
+class RestaurantsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(Faker $faker)
+    {
+        $users = User::all();
+        foreach($users as $user){
+            $newRestaurant = new Restaurant();
+            $newRestaurant->user_id = $user->id;
+            $newRestaurant->rest_name = $faker->firstname();
+            $newRestaurant->open_time = $faker->date();
+            $newRestaurant->close_time = $faker->date();
+            $newRestaurant->phone = $faker->phoneNumber();
+            $newRestaurant->img_cover = $faker->imageUrl(640, 480, 'animals', true);
+            $newRestaurant->img_profile = $faker->imageUrl(640, 480, 'animals', true);
+            $newRestaurant->slug = Str::slug($newRestaurant->rest_name, '-');
+            $newRestaurant->rest_email = $faker->email();
+            $newRestaurant->address = $faker->address();
+            $newRestaurant->save();
+        }
+    }
+}
