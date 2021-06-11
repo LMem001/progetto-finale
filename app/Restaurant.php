87eb12a9046a100aec5d8b982ec9b6e7ec8c618f
Michele Lisignoli <michele@lisignoli.com>
2021-06-11 11:22:26 +0200

    Model Restaurant

---
+++ app/Restaurant.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Restaurant extends Model
+{
+    protected $guarded = [
+        'id', 'restaurant_type'
+    ];
+    public function restaurant_types()
+    {
+        return $this->belongsToMany('App\ResType')
+    }
+    public function foods()
+    {
+        return $this->hasMany('App\Food')
+    }
+}
+
