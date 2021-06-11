bfa00918a48d5c868a678258d1886ff7a1bf9ce6
danielevergani <dvergani94@gmail.com>
2021-06-11 17:14:07 +0200

    restaurant controller (create store)
    solo impostati, da completare

---
+++ app/Http/Controllers/Admin/RestaurantController.php
@@ -0,0 +1,121 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Restaurant;
+use App\ResType;
+use Illuminate\Http\Request;
+use Illuminate\Support\Str;
+
+
+class RestaurantController extends Controller
+{
+    protected $validation = [
+        'rest_name' => 'required|string',
+        'open_time' => 'required|string',
+        'close_time' => 'required|string',
+        'rest_email' => 'required|string',
+        'phone' => 'required|string',
+        'address' => 'required|string'
+    ];
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        $types = ResType::all();
+
+        return view('admin.restaurant.create', compact('types'));    
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Restaurant $restaurant, Request $request)
+    {
+        $validation = $this->validation;
+
+        $request->validate($this->validation);
+
+        $data = $request->all();
+
+        //slug
+        $data['slug'] = Str::slug($data['title'], '-');
+
+        //passare immagine
+        // if(isset($data['image'])){
+        //     $data['image'] = Storage::disk('public')->put('images', $data['image']);
+        // }
+
+        //inserisco dati in db
+        $newPost = Restaurant::create($data); 
+
+        //"collego" i tag
+        if(isset($data['$types'])){
+            $newPost ->restaurant_types()->attach($data['types']);
+        }
+        
+        return redirect()->route('admin.restaurant.create');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,7 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::prefix('admin')->name('admin.')->namespace('Admin')->group(function() {
+    Route::resource('restaurant', 'RestaurantController');
+});
