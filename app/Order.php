2160586141e35cbbe0691ce2c71a94581bb4da49
Davide Vignozzi <davide.vignozzi@gmail.com>
2021-06-11 11:55:25 +0200

    Added Order Model

---
+++ app/Order.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Order extends Model
+{
+    protected $guarded [
+        'id'
+    ];
+
+    public function foods(){
+        return $this->belongsToMany('App\Food');
+    }
+}
