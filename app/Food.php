2a4dcdae2d05997ac7413c1182ce9bf511e71ef5
Luca <memeoluca6@gmail.com>
2021-06-11 15:45:00 +0200

    added foods seeder

--- app/Food.php
+++ app/Food.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Food extends Model
 {
+    protected $table = "foods";
     protected $guarded = [
         'id'
     ];
--- database/seeds/FoodsTableSeeder.php
+++ database/seeds/FoodsTableSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\Restaurant;
+use App\Food;
 
 class FoodsTableSeeder extends Seeder
 {
@@ -11,6 +13,17 @@ class FoodsTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $rests = Restaurant::all();
+        $food_name = ["Arancine", "Pasta con le sarde", "Abbacchio", "Arrosticini"];
+
+        foreach($rests as $rest){
+            for($i = 0; $i < count($food_name); $i++) {
+                $newFood = new Food();
+                $newFood->restaurant_id = $rest->id;
+                $newFood->name = $food_name[$i];
+                $newFood->food_price = "10";
+                $newFood->save();
+            }
+        }
     }
 }
