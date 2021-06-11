b96c9ef10fa975123aa6d7e6a82fd455ee3483dd
Matteo <grecomatteo86@gmail.com>
2021-06-11 10:24:09 +0200

    orders table

---
+++ database/migrations/2021_06_11_081627_create_orders_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+
+            $table->dateTime('pickup_date');
+            $table->boolean('status')->default(0);
+            $table->string('payment_type', 20);
+            $table->smallInteger('total_order');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
