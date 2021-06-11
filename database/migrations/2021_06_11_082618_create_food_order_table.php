66433089707844a14dcb7753d1307f97c4391b24
Davide Vignozzi <davide.vignozzi@gmail.com>
2021-06-11 10:28:35 +0200

    created pivot order food

---
+++ database/migrations/2021_06_11_082618_create_food_order_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFoodOrderTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('food_order', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('food_id')->constrained('foods')->onDelete('cascade');
+            $table->foreignId('order_id')->constrained('orders')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('food_order');
+    }
+}
