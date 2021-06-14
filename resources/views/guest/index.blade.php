6fb6284a02363e53fa2603913d39f7f6c82e1a91
Davide Vignozzi <davide.vignozzi@gmail.com>
2021-06-14 15:46:20 +0200

    Jumbotron

--- resources/views/guest/index.blade.php
+++ resources/views/guest/index.blade.php
@@ -16,6 +16,7 @@
 @endsection
 
 @section('content')
+<<<<<<< Updated upstream
 <div id="app">
     {{-- banner --}}
     <div :class="bannerNone" class="banner">
@@ -27,6 +28,51 @@
     </div>
     {{-- //banner --}}
 </div>
+=======
+    <!-- jumbotron -->
+    <section id="jumbotron">
+        <div class="jumbo-content">
+
+            <h1>
+                Slogan Super Figo
+            </h1>
+
+            <h2>
+                Secondo Slogan Super Figo (solo che un po' più piccolo)
+            </h2>
+
+            <!-- search a restaurant -->
+            <div class="input-jumbotron-container">
+                <form action="">
+                    <input type="search" placeholder="Inserisci il nome del ristorante"/>
+                </form>
+            </div>
+            <!-- search a restaurant -->
+            
+        </div>
+    </section>
+    <!-- end jumboron -->
+
+    <script>
+        function changeBgJb(){
+            const imgBgJb = [
+                'url("https://wallpaperaccess.com/full/462860.jpg")',
+            
+                'url("https://livewallpaperhd.com/wp-content/uploads/2020/09/Food-Picture-Desktop-Backgrounds.jpg")',
+            
+                'url("https://livewallpaperhd.com/wp-content/uploads/2020/09/HD-Wallpaper-Food-Picture.jpg")',
+
+                'url("https://i.pinimg.com/564x/d3/6d/46/d36d462db827833805497d9ea78a1343.jpg")',
+            ]
+            
+            const jumbo = document.getElementById("jumbotron")
+            
+            const bgJb = imgBgJb[Math.floor(Math.random() * imgBgJb.length)];
+            jumbo.style.backgroundImage = bgJb;
+        } 
+        setInterval(changeBgJb, 2000)
+    </script>
+>>>>>>> Stashed changes
 @endsection
 
 @section('script')
