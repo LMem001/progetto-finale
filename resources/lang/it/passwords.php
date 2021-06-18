6fe8035ed90b8ebb0aa4a2bdc145681f1efc04b9
Luca <memeoluca6@gmail.com>
2021-06-18 12:18:19 +0200

    added italian language and setted to default

---
+++ resources/lang/it/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'reset' => 'La tua password è stata cambiata!',
+    'sent' => 'Ti abbiamo inviato il link per cambiare password.',
+    'throttled' => 'Attendi prima di riprovare.',
+    'token' => 'Il link per il cambio della password è scaduto o non esistente.',
+    'user' => "L'email utilizzata non esiste.",
+
+];
