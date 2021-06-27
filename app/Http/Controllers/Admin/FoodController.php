9ac2f729ecb3def07291cac6c62440ba9e26e13f
danielevergani <dvergani94@gmail.com>
2021-06-27 17:21:21 +0200

    change asiatico e allergens not necessary

--- app/Http/Controllers/Admin/FoodController.php
+++ app/Http/Controllers/Admin/FoodController.php
@@ -14,7 +14,7 @@ class FoodController extends Controller
         'name' => 'required|string|max:50',
         'food_price' => 'required|numeric|min:1',
         'tagCourse' => 'required|string|max:25',
-        'allergens' => 'string|max:100'
+        // 'allergens' => 'string|max:100'
     ];
     /**
      * Display a listing of the resource.
